0db3211626fe9df22d431bf5510f82bb49dd06b2
Piyush <itsmepiyush267@gmail.com>
2024-11-01 23:15:00 +0530

    Datatypes

--- variables and datatype.cpp
+++ variables and datatype.cpp
@@ -3,15 +3,30 @@ using namespace std;
 int main()
 {
     int a=20;
+    
     int b=20;
+    
     int c;
+
     c= a + b ;
+    
     cout << c << "\n";
+    
     char grade='A';
+    
     bool ispresent=true;
+    
     float ab=0.14;
+
+    char ofgrade = 'B';
+
+    int pika = grade; // converts the char to int by making displaying its ASCII values
+
     double x =0.49; //default is bool and float is defined
-    cout <<"Pretty basic ik \n";
+    
+    cout <<"Pretty basic ik \n" << "\n" << pika << "\n";
+    
     cout << x << "\n" << a << "\n" << ispresent << "\n" << grade << "\n" << sizeof(ab) ;// size of tells the size of the datatype of variable
+    
     return 0;
 }
