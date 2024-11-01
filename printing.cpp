58e642a72f5c44c85ae5ac2be488ad7137e946ab
Piyush <itsmepiyush267@gmail.com>
2024-11-01 20:35:43 +0530

    ADD

---
+++ printing.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "Apna College" << endl; // or we can use \n instead of endl basically it just starts a new line from where its given  
+    return 0;
+}
---
+++ variables and datatype.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=20;
+    int b=20;
+    int c;
+    c= a + b ;
+    cout << c << "\n";
+    char grade='A';
+    float ab=0.14;
+    cout << a << "\n" << grade << "\n" << sizeof(ab) ;// size of tells the size of the datatype of variable
+    return 0;
+}
