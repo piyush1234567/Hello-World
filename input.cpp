1dfe7ed4bd0804e9a0f5bb0cc9a2300b232c58b9
Piyush <itsmepiyush267@gmail.com>
2024-11-01 23:22:30 +0530

    Input

---
+++ input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout << "Enter your age :" << "\n" ;
+    cin >> age;
+    cout << "your age is " << age << "\n" ;
+    return 0;
+}
